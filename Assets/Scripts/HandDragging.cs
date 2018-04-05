c71d73b329a1867c35fb1fa92a92108c7c814b00
JDZ-3 <jdsmithz42@gmail.com>
2018-04-05 18:08:58 -0400

    First commit

---
+++ Assets/Scripts/HandDragging.cs
@@ -0,0 +1,63 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+using ResultTracker;
+
+namespace DraggedObj
+{
+    public class HandDragging : MonoBehaviour, IManipulationHandler
+    {
+        public Vector3 lastPosition;
+
+        private dataTracker results;
+        private float DragSpeed;
+        private float DragScale;
+        private bool draggingEnabled;
+
+        void Start()
+        {
+            draggingEnabled = true;
+            DragSpeed = 5f;
+            DragScale = 5f;
+            results = GameObject.Find("[Study]").GetComponent<dataTracker>();
+        }
+
+        public void SetDragging(bool enabled)
+        {
+            draggingEnabled = enabled;
+        }
+
+        public void OnManipulationStarted(ManipulationEventData eventData)
+        {
+            InputManager.Instance.PushModalInputHandler(gameObject);
+            lastPosition = transform.position;
+            results.Num_clicks++;
+            results.startInteraction();
+        }
+
+        public void OnManipulationUpdated(ManipulationEventData eventData)
+        {
+            if (draggingEnabled)
+            {
+                Drag(eventData.CumulativeDelta);
+            }
+        }
+
+        public void OnManipulationCompleted(ManipulationEventData eventData)
+        {
+            InputManager.Instance.PopModalInputHandler();
+            results.endInteraction(this.gameObject.name);
+        }
+
+        public void OnManipulationCanceled(ManipulationEventData eventData)
+        {
+            InputManager.Instance.PopModalInputHandler();
+            results.endInteraction(this.gameObject.name);
+        }
+
+        void Drag(Vector3 positon)
+        {
+            var targetPosition = lastPosition + positon * DragScale;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, DragSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/ProjectileLauncher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class ProjectileLauncher : MonoBehaviour, IInputClickHandler
+{
+
+    public GameObject prefab1;
+    public GameObject prefab2;
+    public GameObject prefab3;
+
+    void Start()
+    {
+        InputManager.Instance.PushModalInputHandler(this.gameObject);
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        GameObject projectile = Instantiate(prefab1) as GameObject;
+        projectile.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 0.05f;
+        Rigidbody rb = projectile.GetComponent<Rigidbody>();
+        rb.velocity = Camera.main.transform.forward * 10;
+    }
+
+}
